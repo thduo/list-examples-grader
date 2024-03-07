b946949fcab13b2e98229a0fedcb00d12de66da5
thduo <d9ngo@ucsd.edu>
2024-03-07 08:29:02 -0800

    ADD NEW FILES

--- grade.sh
+++ grade.sh
@@ -8,6 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if ! [ -f student-submission/ListExamples.java ]
+then 
+    echo "Missing needed files"
+    exit 
+
+fi
+echo "continue"
+cp student-submission/ListExamples.java TestListExamples.java grading-area 
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java 
+if [ $? -ne 0 ]
+then 
+    echo "Compilation Error. Double check your code bugs or syntax"
+fi 
+
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
